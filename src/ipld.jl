08d41d944662f5709907c164dce028223c77beff
arhik <arhik23@gmail.com>
2022-10-07 23:18:11 +0530

    Root CID mismatch, needs fix but better version for now

--- src/ipld.jl
+++ src/ipld.jl
@@ -1,13 +1,14 @@
 using ProtoBuf
 
 using MultiFormats
+using Base58
 
 using DataStructures
 
-include(joinpath(@__DIR__, "protos/ipld_pb.jl"))
-include(joinpath(@__DIR__, "protos/unixfsV1_pb.jl"))
+include(joinpath(@__DIR__, "src/protos/ipld_pb.jl"))
+include(joinpath(@__DIR__, "src/protos/unixfsV1_pb.jl"))
 
-path = "test.pdf"
+path = joinpath(@__DIR__, "src/test.pdf")
 
 pdfBytes = read(path)
 
@@ -55,13 +56,14 @@ function fsEncode(blk::Vector{UInt8})
 	take!(io)
 end
 
-function fsEncode(blk::Vector{UInt8}, size, blocksizes)
+function fsEncode(size, blocksizes)
 	io = IOBuffer()
 	e = ProtoEncoder(io)
 	unixfsDict = ProtoBuf.default_values(unixfsV1_pb.Data) |> pairs |> OrderedDict
 	unixfsDict[Symbol("#Type")] = unixfsV1_pb.var"Data.DataType".File # TODO abstract this
 	unixfsDict[:filesize] = size
 	unixfsDict[:blocksizes] = blocksizes
+	# unixfsDict[:fanout] = length(blocksizes)
 	unixfsDict[:mode] = 0x000001a4 # TODO abstract this
 	fs = UnixFSv1(unixfsDict |> NamedTuple)
 	encode(e, getfield(fs, :internal))
@@ -77,7 +79,34 @@ function nodeEncode(blk::Vector{UInt8})
 	take!(io)
 end
 
-function linkEncode(blk::Vector{UInt8})
+function nodeEncode(links::Vector{ipld_pb.PBLink}, data)
+	io = IOBuffer()
+	e = ProtoEncoder(io)
+	encode(e, ipld_pb.PBNode(links, data))
+	seekstart(io)
+	take!(io)
+end
+
+
+function linkEncode(blk::Vector{UInt8}; idx=0, binary=false)
+	io = IOBuffer()
+	e = ProtoEncoder(io)
+	cid = base58encode(blk)
+	# encode(e, cid)
+	# wrappedCID = cidWrap(cid; binary=binary)
+	encode(e, ipld_pb.PBLink(
+		cid,
+		"Links/$idx",
+		2^18
+	))
+	# ipld_pb.PBLink(
+		# cid,
+		# "Links/$idx",
+		# length(blk)
+	# )
+	seekstart(io)
+	take!(io)
+end
 
 function fsDecode()
 	io = IOBuffer()
@@ -88,19 +117,29 @@ end
 
 blockHashed = []
 blockSizes = UInt64[]
+links = ipld_pb.PBLink[]
+
 
 for (idx, block) in enumerate(Base.Iterators.partition(pdfBytes, 2^18))
 	fsBytes = fsEncode(block |> collect)
 	nodeBytes = nodeEncode(fsBytes)
 	hash = multiHash(:sha2_256, nodeBytes)
-	push!(blockHashed, hashWrap(:sha2_256, hash))
+	wrappedmultiHash = hashWrap(:sha2_256, hash)
+	push!(blockHashed, wrappedmultiHash)
 	push!(blockSizes, length(block))
-	push!()
+	push!(links, ipld_pb.PBLink(wrappedmultiHash, "Links/$idx", length(block)))
 end
 
+for hash in blockHashed
+	@info hash |> bytes2hex |> uppercase
+end
 
+for link in links
+	@info link.Hash |> base58encode |> String
+end
 
-fsBytes = fsEncode(vcat(blockHashed...), length(pdfBytes), blockSizes)
-nodeBytes = nodeEncode(fsBytes)
+fsBytes = fsEncode(length(pdfBytes), blockSizes)
+nodeBytes = nodeEncode(links, fsBytes)
 hash = multiHash(:sha2_256, nodeBytes)
-hashWrap(:sha2_256, hash)
+wrappedHash = hashWrap(:sha2_256, hash)
+base58encode(wrappedHash) .|> Char |> String
